f1e325a64db97f1bcd7850df20fb86f5b7f0f6a8
EC2 Default User <ec2-user@ip-172-31-1-20.ap-south-1.compute.internal>
2023-04-26 09:50:25 +0000

    addedfact.c

---
+++ fact.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num, factorial = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.");
+    }
+    else {
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+
+        printf("The factorial of %d is %d", num, factorial);
+    }
+
+    return 0;
+}
