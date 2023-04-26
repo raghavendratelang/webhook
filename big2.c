eb8eefc963959a24ea19ebcfff6bd838df0b7f03
EC2 Default User <ec2-user@ip-172-31-1-20.ap-south-1.compute.internal>
2023-04-26 09:44:56 +0000

    addedbig2.c

---
+++ big2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    if (num1 > num2) {
+        printf("%d is bigger than %d", num1, num2);
+    }
+    else {
+        printf("%d is bigger than %d", num2, num1);
+    }
+
+    return 0;
+}
